6aa3b9e60db2dcab3659903f8557017e404f975e
Saksham Chandel <chandel.sanjeev5@gmail.com>
2025-01-24 01:37:25 +0530

    async await tokio

--- api-project/src/main.rs
+++ api-project/src/main.rs
@@ -1,5 +1,5 @@
 use error_chain::error_chain;
-use std::io::Read;
+use std::{future::Future, io::Read};
 
 error_chain! {
     foreign_links {
@@ -8,13 +8,24 @@ error_chain! {
     }
 }
 
-fn main () -> Result<()>  {
-    let mut res = reqwest::blocking::get("https://httpbin.org/get")?;
-    let mut body = String::new();
-    res.read_to_string(&mut body)?;
 
-    println!("Status :{}", res.status());
-    println!("Header:\n{:#?}", res.headers());
-    println!("Body:\n{}" , body);
+#[tokio::main]
+async fn main() -> Result<()> {
+    let res = reqwest::get("https://httpbin.org/get").await?;
+    println!("Satus:{}" , res.status());
+    println!("Headers:\n{:#?}", res.headers());
+    let body = res.text().await?;
+    println!("Body:\n{}", body);
     Ok(())
 }
+
+// fn main () -> Result<()>  {
+//     let mut res = reqwest::blocking::get("https://httpbin.org/get")?;
+//     let mut body = String::new();
+//     res.read_to_string(&mut body)?;
+
+//     println!("Status :{}", res.status());
+//     println!("Header:\n{:#?}", res.headers());
+//     println!("Body:\n{}" , body);
+//     Ok(())
+// }
